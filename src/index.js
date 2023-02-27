0aaa812bfafea681afab0a454b41cf7ef32fd247
AndreiVauchok2211 <vauchokandrei@gmail.com>
2023-02-27 20:17:48 +0300

    feat: to slove the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = "" + Math.abs(n);
+    let result = Number(str.split("").reverse().join(""));
+    console.log(result);
+    return result;
+};
